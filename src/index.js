8287903ef88b6f20849e123768a9fe9d7d18867f
sleptsovstas <sleptsovstas.v@gmail.com>
2022-04-07 14:23:51 +0900

    initialization

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app/App'
+import './style/style.scss'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
