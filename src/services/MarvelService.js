325234ccbdbcc859e4f189db5a782b80d5b74f68
sleptsovstas <sleptsovstas.v@gmail.com>
2022-04-08 15:09:41 +0900

    reloading data

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-  // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
   _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62'
+  _baseOffset = 210
 
   getResource = async url => {
     let res = await fetch(url)
@@ -13,9 +13,9 @@ class MarvelService {
     return await res.json()
   }
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     )
     return res.data.results.map(this._transformCharacter)
   }
